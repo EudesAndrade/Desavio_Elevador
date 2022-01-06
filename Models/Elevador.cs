d29d8bfb80cd540e6d4e614f06e7b972c9e67838
Eudes Gomes de Andrade <eudes.andrade@TRANSIRE.COM>
2022-01-05 22:07:28 -0400

    Projeto_Elevador

---
+++ Models/Elevador.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Models
+{
+    public class Elevador
+    {
+        int total_Andares;
+        int capac_Pessoas;
+        int pessoas_Elevador;
+        int andar_Atual;
+        public int AndarCorrent = 0;
+
+        //Método Inicializar serve para dar boas vindas no elevador!
+        public void Inicializar()
+        {
+            Console.WriteLine("  Seja bem vindo!!!\n");
+           // Console.WriteLine("                       ");
+            Console.WriteLine("  Qual é a capacidade do elevador?\n");
+            capac_Pessoas = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("  Quantos andares tem o prédio?");
+            total_Andares = int.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+
+        //Método Entrar atribui uma pessoa no elevador caso ainda caiba alguém!
+        public void Entrar() 
+        {
+            if (pessoas_Elevador < capac_Pessoas)
+            {
+                pessoas_Elevador++;
+                Console.WriteLine( pessoas_Elevador + " Pessoas no Elevador.");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, Elevador atingiu a capacidade máxima.");
+            }
+
+        }
+
+        //Método Sair subtrai uma pessoa no elevador caso não esteja vazio!
+        public int Sair(int andarDigitado) 
+        {
+            if (pessoas_Elevador > 0)
+            {
+                pessoas_Elevador--;
+                andarDigitado = 1;
+                Console.WriteLine(pessoas_Elevador + " Pessoas no Elevador.");
+            }
+            else if (pessoas_Elevador == 0 || pessoas_Elevador < 0)
+            {
+                Console.WriteLine("!! Elevador está vazio !!");
+                Console.WriteLine("!! obrigado por usar o Elevador!!");
+                andarDigitado = 2;
+
+            }
+            return andarDigitado;
+        }
+
+        //Método Subir serve para subir um andar por vez no elevador!
+        public void Subir()
+        {
+            if (andar_Atual >= total_Andares)
+            {
+                Console.WriteLine("Voce já está no último andar, deseja descer ou subir?");
+            }
+            else
+            {
+                andar_Atual++;
+                //Stop(andar_Atual);
+                Console.WriteLine("Voce está no andar: " + andar_Atual);
+            }
+
+
+        }
+
+        //Método Descer serve para descer um andar por vez no elevador!
+        public void Descer()
+        {
+            if (andar_Atual == 0)
+            {
+                Console.WriteLine("Voce já está no Térreo, deseja subir ou sair?");
+            }
+            else
+            {
+                andar_Atual--;
+                //Stop(andar_Atual);
+                Console.WriteLine("Voce já está no andar: " + andar_Atual);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using ProjetoElevador.Models;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        //private const string Sair = "3";
+
+        static void Main(string[] args)
+        {
+
+        Start:
+
+            Elevador elevador = new Elevador();
+
+            elevador.Inicializar();
+            int sair = 0;
+
+            Console.WriteLine(@"Deseja entrar no Elevador:
+                    1 - Sim
+                    2 - Não
+                            ");
+            int entarElevador = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (entarElevador)
+            {
+                case 1: elevador.Entrar(); break;
+                case 2:
+                    Console.WriteLine("---Até logo---");
+                    Environment.Exit(entarElevador);
+                    break;
+                default:
+                    Console.WriteLine("Escolha Inválida, escolha uma opção novamente");
+                    goto Start;
+            }
+
+        Final:
+
+            Console.WriteLine(@"Favor digite o que deseja fazer:
+                    1 - Subir
+                    2 - Descer
+                    3 - Sair
+                                ");
+
+            int andar = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (andar)
+            {
+                case 1: elevador.Subir(); break;
+                case 2: elevador.Descer(); break;
+                case 3:
+                    elevador.Sair(sair);
+                    sair = elevador.Sair(sair);
+
+                    if (sair == 1)
+                    {
+                        goto Final;
+                    }
+                    if (sair == 2)
+                    {
+                        Environment.Exit(andar);
+
+                    }; break;
+                default:
+                    Console.WriteLine("Escolha Inválida, escolha uma opção novamente");
+                    goto Final;
+            }
+
+            goto Final;
+        }
+    }
+}
